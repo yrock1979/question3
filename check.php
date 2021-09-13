1716d930248d96f316a4caa8a30ad48ff48fefb4
yrock1979 <88130247+yrock1979@users.noreply.github.com>
2021-09-14 05:41:23 +0900

    Initial commit

---
+++ check.php
@@ -0,0 +1,18 @@
+<?php
+
+    echo $_GET['name'];
+
+
+// <!DOCTYPE html>
+// <html lang="jp">
+// <head>
+//     <meta charset="UTF-8">
+//     <meta http-equiv="X-UA-Compatible" content="IE=edge">
+//     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+//     <title>Document</title>
+// </head>
+// <body>
+//     <h1>入力内容確認</h1>
+//     <p>お名前</p>
+// </body>
+// </html>
---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+</head>
+<body>
+  <form method="get" action="check.php">
+    <div>
+      <label for="">お名前</label>
+      <input type="text" name="name" >
+    </div>
+    <input type="submit" value="送信">
+  </form>
+</body>
+</html>
